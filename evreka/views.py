b0c156c2c9be65db9152226e57ef9d79c5c4863c
AyseBerna <ayseberna.aksoy@gmail.com>
2021-11-14 17:29:05 +0300

    deleted unnecessary lines

--- evreka/views.py
+++ evreka/views.py
@@ -6,22 +6,7 @@ from datetime import date, datetime, timedelta, timezone
 def index(request):
     time_threshold = datetime.now() - timedelta(hours=48)
     navigationRecords = NavigationRecord.objects.filter(datetime__gte=time_threshold)
-    print('======', navigationRecords.values())
-    # for nr in navigationRecords.values():
-    #     vehicle = Vehicle.objects.filter(id=nr['vehicle_id'])
-    #     nr['vehicle_plate'] = vehicle
     return render(request, "index.html", {"navigationRecords": navigationRecords})
 
-#   navigationRecords = NavigationRecord.objects.all()
-#   return render(request, "index.html", {"navigationRecord": navigationRecords})
-# def addNavigationRecord(request):
-#     if request.method == "GET":
-#         return redirect("/"
-#     else:
-#         datetime = request.POST.get("datetime")
-
-#         newNavigationRecord = NavigationRecord(datetime = datetime)
-#         newNavigationRecord.save()
-#         return redirect("/")
 
 
